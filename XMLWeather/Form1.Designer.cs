6c4c83acb7592fdcd5b4710435388ef83918c3b9
JessNguy <jessnguy642@ed.amdsb.ca>
2016-04-20 10:07:33 -0400

    Trying to put values to the labels

--- XMLWeather/Form1.Designer.cs
+++ XMLWeather/Form1.Designer.cs
@@ -30,27 +30,294 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.cityOutput = new System.Windows.Forms.Label();
+            this.labelTemp = new System.Windows.Forms.Label();
+            this.labelWind = new System.Windows.Forms.Label();
+            this.labelMax = new System.Windows.Forms.Label();
+            this.labelMin = new System.Windows.Forms.Label();
+            this.labelMax2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelCloud1 = new System.Windows.Forms.Label();
+            this.labelCloud2 = new System.Windows.Forms.Label();
+            this.labelCountry = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.labelType = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.buttonDay1 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // cityOutput
             // 
             this.cityOutput.AutoSize = true;
-            this.cityOutput.Location = new System.Drawing.Point(61, 120);
+            this.cityOutput.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cityOutput.Location = new System.Drawing.Point(225, 98);
             this.cityOutput.Name = "cityOutput";
-            this.cityOutput.Size = new System.Drawing.Size(35, 13);
+            this.cityOutput.Size = new System.Drawing.Size(70, 38);
             this.cityOutput.TabIndex = 0;
-            this.cityOutput.Text = "label1";
+            this.cityOutput.Text = "City";
+            // 
+            // labelTemp
+            // 
+            this.labelTemp.AutoSize = true;
+            this.labelTemp.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTemp.Location = new System.Drawing.Point(267, 268);
+            this.labelTemp.Name = "labelTemp";
+            this.labelTemp.Size = new System.Drawing.Size(86, 38);
+            this.labelTemp.TabIndex = 1;
+            this.labelTemp.Text = "Temp";
+            // 
+            // labelWind
+            // 
+            this.labelWind.AutoSize = true;
+            this.labelWind.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWind.Location = new System.Drawing.Point(434, 395);
+            this.labelWind.Name = "labelWind";
+            this.labelWind.Size = new System.Drawing.Size(113, 30);
+            this.labelWind.TabIndex = 2;
+            this.labelWind.Text = "Wind Dir.";
+            // 
+            // labelMax
+            // 
+            this.labelMax.AutoSize = true;
+            this.labelMax.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMax.Location = new System.Drawing.Point(203, 395);
+            this.labelMax.Name = "labelMax";
+            this.labelMax.Size = new System.Drawing.Size(56, 30);
+            this.labelMax.TabIndex = 3;
+            this.labelMax.Text = "Max";
+            // 
+            // labelMin
+            // 
+            this.labelMin.AutoSize = true;
+            this.labelMin.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMin.Location = new System.Drawing.Point(323, 395);
+            this.labelMin.Name = "labelMin";
+            this.labelMin.Size = new System.Drawing.Size(49, 30);
+            this.labelMin.TabIndex = 4;
+            this.labelMin.Text = "Min";
+            // 
+            // labelMax2
+            // 
+            this.labelMax2.AutoSize = true;
+            this.labelMax2.Location = new System.Drawing.Point(510, 255);
+            this.labelMax2.Name = "labelMax2";
+            this.labelMax2.Size = new System.Drawing.Size(56, 13);
+            this.labelMax2.TabIndex = 5;
+            this.labelMax2.Text = "Max day 2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(510, 287);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Min day 2";
+            // 
+            // labelCloud1
+            // 
+            this.labelCloud1.AutoSize = true;
+            this.labelCloud1.Location = new System.Drawing.Point(510, 307);
+            this.labelCloud1.Name = "labelCloud1";
+            this.labelCloud1.Size = new System.Drawing.Size(63, 13);
+            this.labelCloud1.TabIndex = 7;
+            this.labelCloud1.Text = "Cloud day 1";
+            // 
+            // labelCloud2
+            // 
+            this.labelCloud2.AutoSize = true;
+            this.labelCloud2.Location = new System.Drawing.Point(510, 228);
+            this.labelCloud2.Name = "labelCloud2";
+            this.labelCloud2.Size = new System.Drawing.Size(63, 13);
+            this.labelCloud2.TabIndex = 8;
+            this.labelCloud2.Text = "Cloud day 2";
+            // 
+            // labelCountry
+            // 
+            this.labelCountry.AutoSize = true;
+            this.labelCountry.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCountry.Location = new System.Drawing.Point(261, 60);
+            this.labelCountry.Name = "labelCountry";
+            this.labelCountry.Size = new System.Drawing.Size(117, 38);
+            this.labelCountry.TabIndex = 9;
+            this.labelCountry.Text = "Country";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(254, 147);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 94);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDate.Location = new System.Drawing.Point(27, 390);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(66, 35);
+            this.labelDate.TabIndex = 11;
+            this.labelDate.Text = "date";
+            // 
+            // labelType
+            // 
+            this.labelType.AutoSize = true;
+            this.labelType.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelType.Location = new System.Drawing.Point(239, 342);
+            this.labelType.Name = "labelType";
+            this.labelType.Size = new System.Drawing.Size(139, 30);
+            this.labelType.TabIndex = 12;
+            this.labelType.Text = "Type of day";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(536, 395);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 30);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Wind Spd.";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(163, 395);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(40, 30);
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(277, 395);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(40, 30);
+            this.pictureBox3.TabIndex = 15;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(397, 395);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(40, 30);
+            this.pictureBox4.TabIndex = 16;
+            this.pictureBox4.TabStop = false;
+            // 
+            // buttonDay1
+            // 
+            this.buttonDay1.Location = new System.Drawing.Point(16, 449);
+            this.buttonDay1.Name = "buttonDay1";
+            this.buttonDay1.Size = new System.Drawing.Size(96, 57);
+            this.buttonDay1.TabIndex = 17;
+            this.buttonDay1.Text = "Day 1";
+            this.buttonDay1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(107, 449);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 57);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Day 1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(199, 449);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 57);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Day 1";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(292, 449);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 57);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "Day 1";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(384, 449);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(96, 57);
+            this.button4.TabIndex = 21;
+            this.button4.Text = "Day 1";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(476, 449);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(96, 57);
+            this.button5.TabIndex = 22;
+            this.button5.Text = "Day 1";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(569, 449);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(96, 57);
+            this.button6.TabIndex = 23;
+            this.button6.Text = "Day 1";
+            this.button6.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(244, 269);
+            this.ClientSize = new System.Drawing.Size(675, 526);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonDay1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelType);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelCountry);
+            this.Controls.Add(this.labelCloud2);
+            this.Controls.Add(this.labelCloud1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelMax2);
+            this.Controls.Add(this.labelMin);
+            this.Controls.Add(this.labelMax);
+            this.Controls.Add(this.labelWind);
+            this.Controls.Add(this.labelTemp);
             this.Controls.Add(this.cityOutput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Central Weather";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -59,7 +326,29 @@
         #endregion
 
         private System.Windows.Forms.Label cityOutput;
-
+        private System.Windows.Forms.Label labelTemp;
+        private System.Windows.Forms.Label labelWind;
+        private System.Windows.Forms.Label labelMax;
+        private System.Windows.Forms.Label labelMin;
+        private System.Windows.Forms.Label labelMax2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelCloud1;
+        private System.Windows.Forms.Label labelCloud2;
+        private System.Windows.Forms.Label labelCountry;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.Label labelType;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button buttonDay1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- XMLWeather/Form1.cs
+++ XMLWeather/Form1.cs
@@ -34,8 +34,9 @@ namespace XMLWeather
             string currentFile = "http://api.openweathermap.org/data/2.5/weather?q=Stratford,CA&mode=xml&units=metric&appid=3f2e224b815c0ed45524322e145149f0";
             string forecastFile = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Stratford,CA&mode=xml&units=metric&cnt=7&appid=3f2e224b815c0ed45524322e145149f0";
             
-            client.DownloadFile(currentFile, "WeatherData.xml");
-            client.DownloadFile(forecastFile, "WeatherData7Day.xml");
+            //download this web address file and save it into weatherdata document file
+              //client.DownloadFile(currentFile, "WeatherData.xml");
+              //client.DownloadFile(forecastFile, "WeatherData7Day.xml");
         }
 
         private void ExtractCurrent()
@@ -51,12 +52,103 @@ namespace XMLWeather
             foreach (XmlNode child in parent.ChildNodes)
             {
                 // TODO if the "city" element is found display the value of it's "name" attribute
+                if (child.Name == "city")
+                {
+                    cityOutput.Text = child.Attributes["name"].Value;
+                }
+              
+                if(child.Name =="temperature")
+                {
+                    labelTemp.Text = child.Attributes["value"].Value;
+                }
+                if(child.Name == "wind")
+                {
+                    foreach(XmlNode grandChild in child.ChildNodes)
+                    {
+                        if(grandChild.Name == "speed")
+                        {
+                            labelWind.Text = grandChild.Attributes["name"].Value;
+                        }
+                    }
+                }
             }
         }
-              
+
         private void ExtractForecast()
         {
-                     
+            XmlDocument doc = new XmlDocument();
+            doc.Load("WeatherData7Day.xml");
+
+            //create a node variable to represent the parent element
+            XmlNode parent;
+            parent = doc.DocumentElement;
+
+            int day = 1;
+            //check each child of the parent element
+            foreach (XmlNode child in parent.ChildNodes)
+            {
+                if (child.Name == "location")
+                {
+                    foreach (XmlNode grandChild in child.ChildNodes)
+                    {
+                        if (grandChild.Name == "country")
+                        {
+                            labelCountry.Text = grandChild.InnerText;
+                        }
+                    }
+
+                }
+
+                if (child.Name == "forecast")
+                {
+                    foreach (XmlNode grandChild in child.ChildNodes)
+                    {
+                        foreach (XmlNode greatGrandChild in grandChild.ChildNodes)
+                        {
+                            //if (greatGrandChild.Name == "symbol var")
+                            //{
+                            //    labelType.Text = greatGrandChild.Attributes["name"].Value;
+                            //}
+                            if (greatGrandChild.Name == "temperature")
+                            {
+                                switch (day)
+                                {
+                                    case 1:
+                                        labelMax.Text = greatGrandChild.Attributes["max"].Value;
+                                        labelMin.Text = greatGrandChild.Attributes["min"].Value;
+
+                                        break;
+                                    case 2:
+                                        labelMax2.Text = greatGrandChild.Attributes["max"].Value;
+                                        label1.Text = greatGrandChild.Attributes["min"].Value;
+
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            }
+
+                            if (greatGrandChild.Name == "clouds")
+                            {
+                                switch (day)
+                                {
+                                    case 1:
+                                        labelCloud1.Text = greatGrandChild.Attributes["value"].Value;
+                                        day++;
+                                        break;
+                                    case 2:
+                                        labelCloud2.Text = greatGrandChild.Attributes["value"].Value;
+                                        day++;
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            }
+                       
+                        }
+                    }
+                }
+            }
         }
 
 
@@ -65,5 +157,6 @@ namespace XMLWeather
             
         }
 
+  
     }
 }
